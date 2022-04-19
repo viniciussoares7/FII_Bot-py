2e0196d305be6da5c3dbdcb196d57f0d38e94b7e
Vinicius <vinicius-a-soares@outlook.com>
2022-04-19 13:29:00 -0300

    data frame to list and sucessefully acess to the fundamentus' site

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from argparse import Action
+from ctypes import wstring_at
+from multiprocessing.connection import wait
+from operator import index
+from unittest import skip
 from numpy import inner
 import numpy as np
+from openpyxl import load_workbook, Workbook
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -63,7 +68,34 @@ class Charles:
             self.excel_data = pd.read_csv(
                 r'C:/Users/vinic/Downloads/fundosListados.csv', sep=';', encoding='latin-1')
             df = pd.DataFrame(self.excel_data, columns=[r'Código'])
-            print(df)
+            # print(df)
+        # ------------------- Inserindo dados no excel -------------------------------------------------
+        df.to_excel("C:/bots/Charles/fundos_imob.xlsx")
+        df2 = pd.read_excel("C:/bots/Charles/fundos_imob.xlsx")
+        df2.drop(['Unnamed: 0'], axis=1, inplace=True)
+        #writer = pd.ExcelWriter('C:/bots/Charles/fundos_imob.xlsx')
+        #df2.to_excel(writer, 'Sheet1')
+        # writer.save()
+        # -------------------------------------------------------------------------------------------------
+        #browser = webdriver.Chrome()
+        #df2 = pd.DataFrame(columns=[r'Código'])
+        baseUrl = 'https://fundamentus.com.br/detalhes.php?papel='
+
+        fii_list = df2.values.tolist()
+        #values = []
+        for fii in fii_list:
+            fii = fii[0]
+
+            self.driver.get(baseUrl + fii + '11')
+            sleep(1)
+            msg = self.driver.find_element(
+                by=By.CSS_SELECTOR, value='body > div.center > div.conteudo.clearfix > div > div > h1').text
+            if msg == 'Nenhum papel encontrado':
+                print('Nenhum papel encontrado')
+                continue
+            else:
+                #pvp = self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[2]/table[3]/tbody/tr[4]/td[4]/span').inner_text
+                print(fii)
 
 
 start = Charles()
